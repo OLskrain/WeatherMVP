0e09ea1e0f1b94db27e3b6d54e4ca05a0e34896e
OLskrain <Olskrain2@yandex.ru>
2019-02-12 14:08:14 +0300

    added BottomNavigationView

--- app/src/main/java/com/example/olskr/weathermvp/MainActivity.java
+++ app/src/main/java/com/example/olskr/weathermvp/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.olskr.weathermvp;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.message)
+    TextView mTextMessage;
+    @BindView(R.id.navigation)
+    BottomNavigationView navigation;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home); //TODO: Перенести подобный код взаимодействия в Presenter
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_places);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_other);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
